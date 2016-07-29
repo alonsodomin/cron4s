d67a28e1cbf65a4a4b735ced8e4afc88a3d1a490
Antonio Alonso Dominguez <alonso.domin@gmail.com>
2016-07-29 21:18:40 +0100

    Fix parser spec

--- core/shared/src/test/scala/cron4s/parser/ExprParsersSpec.scala
+++ core/shared/src/test/scala/cron4s/parser/ExprParsersSpec.scala
@@ -22,32 +22,32 @@ object ExprParsersSpec extends Properties("ExprParsers") with ExprParsers with E
   }
 
   /** Utility method to help with type inference */
-  def verifyScalar[F <: CronField](parser: Parser[ConstExpr[F]], input: String)(verify: ConstExpr[F] => Boolean): Boolean =
+  def verifyConst[F <: CronField](parser: Parser[ConstExpr[F]], input: String)(verify: ConstExpr[F] => Boolean): Boolean =
     verifyParsed[F, ConstExpr[F]](parser, input)(verify)
 
   property("Should be able to parse minutes") = forAll(Gen.choose(0, 59)) {
-    x => verifyScalar(minute, x.toString) { expr => expr.value == x }
+    x => verifyConst(minute, x.toString) { expr => expr.value == x }
   }
 
   property("Should be able to parse hours") = forAll(Gen.choose(0, 23)) {
-    x => verifyScalar(hour, x.toString) { expr => expr.value == x }
+    x => verifyConst(hour, x.toString) { expr => expr.value == x }
   }
 
   property("Should be able to parse days of month") = forAll(Gen.choose(1, 31)) {
-    x => verifyScalar(dayOfMonth, x.toString) { expr => expr.value == x }
+    x => verifyConst(dayOfMonth, x.toString) { expr => expr.value == x }
   }
 
   property("Should be able to parse numeric months") = forAll(Gen.choose(1, 12)) {
-    x => verifyScalar(month, x.toString) { expr => expr.value == x && expr.textValue.isEmpty }
+    x => verifyConst(month, x.toString) { expr => expr.value == x && expr.textValue.isEmpty }
   }
   property("Should be able to parse named months") = forAll(Gen.oneOf(MonthsUnit.namedValues)) {
-    x => verifyScalar(month, x) { expr =>
-      expr.textValue.contains(x) && expr.matches.matches(MonthsUnit.namedValues.indexOf(x) + 1)
+    x => verifyConst(month, x) { expr =>
+      expr.textValue.contains(x) && expr.matches(MonthsUnit.namedValues.indexOf(x) + 1)
     }
   }
 
   property("Should be able to parse numeric days of week") = forAll(Gen.choose(0, 6)) {
-    x => verifyScalar(dayOfWeek, x.toString) { _.value == x }
+    x => verifyConst(dayOfWeek, x.toString) { _.value == x }
   }
 
 }
