1b5254930dc5b07520fd21f68539011e1b8e14d9
Antonio Alonso Dominguez <alonso.domin@gmail.com>
2016-08-04 01:07:15 +0100

    Verify steps from outside the range in the units

--- core/shared/src/test/scala/cron4s/CronUnitSpec.scala
+++ core/shared/src/test/scala/cron4s/CronUnitSpec.scala
@@ -17,11 +17,25 @@ object CronUnitSpec extends Properties("CronUnit") with BaseGenerators {
     case (unit, value) => unit.indexOf(value).get == unit.range.indexOf(value)
   }
 
+  val stepsFromOutsideRange = for {
+    unit      <- cronUnits
+    fromValue <- arbitrary[Int] if fromValue < unit.min || fromValue > unit.max
+    stepSize  <- arbitrary[Int]
+  } yield (unit, fromValue, stepSize)
+
+  property("stepping from outside the range yields None") = forAll(stepsFromOutsideRange) {
+    case (unit, fromValue, stepSize) => unit.step(fromValue, stepSize).isEmpty
+  }
+
   val stepsFromWithinRange = for {
     unit      <- cronUnits
     fromValue <- Gen.choose(unit.min, unit.max)
     stepSize  <- arbitrary[Int]
   } yield (unit, fromValue, stepSize)
+
+  property("stepping with a zero step size returns the input") = forAll(stepsFromWithinRange) {
+    case (unit, fromValue, _) => unit.step(fromValue, 0).contains(fromValue -> 0)
+  }
 
   property("stepping from inside the range yields sequential values") = forAll(stepsFromWithinRange) {
     case (unit, fromValue, stepSize) =>
