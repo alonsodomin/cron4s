c0ee1c9798d328a81aa3ba1196808f52a3f63d6f
Antonio Alonso Dominguez <alonso.domin@gmail.com>
2016-08-07 12:37:38 +0100

    Remove internal shapeless helper class in `CronExpr`

--- core/shared/src/main/scala/cron4s/expr/CronExpr.scala
+++ core/shared/src/main/scala/cron4s/expr/CronExpr.scala
@@ -2,18 +2,10 @@ package cron4s.expr
 
 import cron4s.{CronField, CronUnit}
 
-import shapeless.HList
-import shapeless.ops.hlist.Selector
-
 /**
   * Created by domingueza on 29/07/2016.
   */
 final case class CronExpr(repr: CronExprRepr) {
-
-  class GetField[F <: CronField] {
-    def apply[L <: HList](expr: L)(implicit selector: Selector[L, Expr[F]]): Expr[F] =
-      expr.select[Expr[F]]
-  }
 
   lazy val minutes = repr.select[Expr[CronField.Minute.type]]
   lazy val hours = repr.select[Expr[CronField.Hour.type]]
