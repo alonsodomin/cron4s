d9621df7461ba7a6dc42c09eae20266398945a29
Antonio Alonso Dominguez <alonso.domin@gmail.com>
2016-08-29 11:37:24 +0100

    Adjust DayOfWeek on calendar cron expression spec

--- tests/jvm/src/test/scala/cron4s/japi/CalendarCronExprSpec.scala
+++ tests/jvm/src/test/scala/cron4s/japi/CalendarCronExprSpec.scala
@@ -3,6 +3,7 @@ package cron4s.japi
 import java.time.{LocalDateTime, ZoneOffset}
 import java.util.{Calendar, TimeZone}
 
+import cron4s.CronField
 import cron4s.testkit.ExtendedCronExprTestKit
 
 import calendar._
@@ -14,11 +15,12 @@ class CalendarCronExprSpec extends ExtendedCronExprTestKit[Calendar] {
 
   def createDateTime(minutes: Int, hours: Int, dayOfMonth: Int, month: Int, dayOfWeek: Int): Calendar = {
     val cal = Calendar.getInstance(TimeZone.getTimeZone("UTC"))
+
     val timeMillis = LocalDateTime.of(2016, month, dayOfMonth, hours, minutes).
       toInstant(ZoneOffset.UTC).toEpochMilli
     cal.setTimeInMillis(timeMillis)
 
-    cal
+    JavaCalendarAdapter.set(cal, CronField.DayOfWeek, dayOfWeek).get
   }
 
 }
