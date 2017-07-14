a8237e24228de196a54d7db1d3fcb2d7327d373f
Antonio Alonso Dominguez <alonso.domin@gmail.com>
2017-07-14 11:48:38 +0100

    Add cats' Either syntax for compatibility with Scala 2.11

--- core/shared/src/main/scala/cron4s/datetime/DateTimeNode.scala
+++ core/shared/src/main/scala/cron4s/datetime/DateTimeNode.scala
@@ -66,6 +66,7 @@ trait DateTimeNode[E[_ <: CronField], F <: CronField] {
     * @return a date-time that is an amount of given steps from the given one
     */
   def stepIn[DateTime](expr: E[F], DT: IsDateTime[DateTime])(dateTime: DateTime, step: Int): Option[DateTime] = {
+    import cats.syntax.either._
     for {
       current  <- DT.get(dateTime, expr.unit.field)
       newValue <- expr.step(current, step).map(_._1)
