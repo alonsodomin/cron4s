298ff4113522b3c8b82cca52787bb17021fd73e2
Scala steward <43047562+scala-steward@users.noreply.github.com>
2018-10-18 11:00:50 +0200

    Update discipline to 0.10.0 (#110)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -14,7 +14,7 @@ object Dependencies {
     val fastparse   = "1.0.0"
     val scalacheck  = "1.14.0"
     val scalatest   = "3.0.5"
-    val discipline  = "0.9.0"
+    val discipline  = "0.10.0"
     val catalysts   = "0.6"
 
     val jodaTime      = "2.9.9"
