305c01585a130bec345ebac70ae586112b188065
Antonio Alonso Dominguez <antoniodominguez@rentalcars.com>
2019-03-22 19:54:03 +0000

    Back to default matrix expansion

--- scripts/travis-setup.sh
+++ scripts/travis-setup.sh
@@ -5,8 +5,7 @@ rvm use 2.2.8 --install --fuzzy
 gem update --system
 
 echo "Installing Jekyll..."
-gem install sass
 gem install jekyll -v 3.2.1
 
-echo "Installing JSDom..."
-npm install jsdom
+echo "Installing Codecov..."
+pip install --user codecov
