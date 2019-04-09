aafb1425af3116783eab007979c92b2ef0fbfea9
Antonio Alonso Dominguez <antoniodominguez@rentalcars.com>
2019-04-09 08:08:25 +0100

    Bring contextual back

--- modules/core/shared/src/main/scala/cron4s/syntax/string.scala
+++ modules/core/shared/src/main/scala/cron4s/syntax/string.scala
@@ -21,61 +21,19 @@ import cats.syntax.either._
 
 import contextual._
 
-private[cron4s] sealed trait CronInterpolatorCtx extends Context
-private[cron4s] object CronInterpolatorCtx {
-  case object Plain    extends CronInterpolatorCtx
-  case object ToString extends CronInterpolatorCtx
-}
-
 private[syntax] trait CronStringSyntax {
-  import CronInterpolatorCtx._
-
+  
   implicit def toCronStringInterpolator(sc: StringContext): CronStringContext =
     new CronStringContext(sc)
 
-  implicit val embedCronStrings = CronStringInterpolator.embed[String](
-    Case(Plain, Plain)(identity)
-  )
-
 }
 
-object CronStringInterpolator extends Interpolator {
-  type Output      = CronExpr
-  type Input       = String
-  type ContextType = CronInterpolatorCtx
+object CronStringInterpolator extends Verifier[CronExpr] {
 
   def check(input: String) = Cron.parse(input).leftMap {
     case parseErr: ParseFailed => parseErr.position -> parseErr.getMessage
     case other: Error          => 0                 -> other.getMessage
   }
-
-  def contextualize(interpolation: StaticInterpolation): Seq[ContextType] =
-    interpolation.parts.flatMap {
-      case hole @ Hole(_, _) => Seq(CronInterpolatorCtx.Plain)
-      case _                 => Nil
-    }
-
-  // def contextualize(interpolation: StaticInterpolation): Seq[ContextType] = {
-  //   interpolation.parts.foreach {
-  //     case lit @ Literal(_, string) =>
-  //       println(s"lit: $string")
-  //       check(string) match {
-  //         case Left((pos, error)) =>
-  //           interpolation.abort(lit, pos, error)
-  //         case Right(_) =>
-  //           println("Incomplete parsing completed??")
-  //           ()
-  //       }
-
-  //     case hole @ Hole(_, _) =>
-  //       println(s"hole: $hole")
-  //       interpolation.abort(hole, "substitutions are not permitted")
-  //   }
-  //   Nil
-  // }
-
-  def evaluate(contextual: RuntimeInterpolation): CronExpr =
-    Cron.unsafeParse(contextual.parts.mkString)
 
 }
 
