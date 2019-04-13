c474a9225798f258d0b5112ebf6441573bb9a09f
Antonio Alonso Dominguez <antoniodominguez@rentalcars.com>
2019-04-13 11:53:04 +0100

    Draft a potential future AST

--- modules/core/shared/src/main/scala/cron4s/expr2/nodes.scala
+++ modules/core/shared/src/main/scala/cron4s/expr2/nodes.scala
@@ -17,23 +17,127 @@
 package cron4s
 package expr
 
-import cron4s.base.Step
+import cats.data.NonEmptyList
+
+import cron4s.base._
 import cron4s.datetime.IsDateTime
 
 sealed trait Constraint[F <: CronField] {
-  def step[DT: IsDateTime](from: DT, step: Step): Either[StepError, (Int, DT)]
+  def unit: CronUnit[F]
+}
+sealed trait EnumerableConstraint[F <: CronField] extends Constraint[F]
+sealed trait DivisibleConstraint[F <: CronField]  extends Constraint[F]
+
+case class EachConstraint[F <: CronField](unit: CronUnit[F])
+    extends Constraint[F] with DivisibleConstraint[F]
+object EachConstraint {
+
+  implicit def eachConstraintRange[F <: CronField](
+      implicit R: Enumerated[CronUnit[F]]
+  ): Enumerated[EachConstraint[F]] = new Enumerated[EachConstraint[F]] {
+    def range(c: EachConstraint[F]) = R.range(c.unit)
+  }
+
 }
 
-case class EachConstraint[F <: CronField](unit: CronUnit[F]) extends Constraint[F] {
-  def step[DT](from: DT, step: Step)(
-      implicit DT: IsDateTime[DT]
-  ): Either[StepError, (Int, DT)] =
-    if (!DT.supportedFields(from).contains(unit.field)) Left(UnsupportedField(unit.field))
-    else {
-      for {
-        currValue <- DT.get(from, unit.field)
+case class AnyConstraint[F <: CronField](unit: CronUnit[F]) extends Constraint[F]
+object AnyConstraint {
+  implicit def anyConstraintRange[F <: CronField](
+      implicit R: Enumerated[CronUnit[F]]
+  ): Enumerated[AnyConstraint[F]] = new Enumerated[AnyConstraint[F]] {
+    def range(c: AnyConstraint[F]) = R.range(c.unit)
+  }
+}
 
-      } yield ()
-      ???
+case class ConstConstraint[F <: CronField](value: Int, unit: CronUnit[F])
+    extends Constraint[F] with EnumerableConstraint[F]
+object ConstConstraint {
+  implicit def constConstraintRange[F <: CronField](
+      implicit R: Enumerated[CronUnit[F]]
+  ): Enumerated[ConstConstraint[F]] = new Enumerated[ConstConstraint[F]] {
+    def range(c: ConstConstraint[F]) = Vector(c.value)
+  }
+}
+
+case class BetweenConstraint[F <: CronField](
+    begin: ConstConstraint[F],
+    end: ConstConstraint[F],
+    unit: CronUnit[F]
+) extends Constraint[F] with EnumerableConstraint[F] with DivisibleConstraint[F]
+object BetweenConstraint {
+  implicit def betweenConstraintRange[F <: CronField](
+      implicit R: Enumerated[CronUnit[F]]
+  ): Enumerated[BetweenConstraint[F]] = new Enumerated[BetweenConstraint[F]] {
+    def range(c: BetweenConstraint[F]) = {
+      val min = Math.min(c.begin.value, c.end.value)
+      val max = Math.max(c.begin.value, c.end.value)
+      min to max
     }
+  }
 }
+
+case class SeveralConstraint[F <: CronField](
+    head: EnumerableConstraint[F],
+    tail: NonEmptyList[EnumerableConstraint[F]],
+    unit: CronUnit[F]
+) extends Constraint[F] with DivisibleConstraint[F] {
+  lazy val values: NonEmptyList[EnumerableConstraint[F]] = head :: tail
+}
+object SeveralConstraint {
+  implicit def severalConstraintRange[F <: CronField](
+      implicit R: Enumerated[EnumerableConstraint[F]]
+  ): Enumerated[SeveralConstraint[F]] = new Enumerated[SeveralConstraint[F]] {
+    def range(c: SeveralConstraint[F]) =
+      c.values.toList.view.flatMap(_.range).distinct.sorted.toIndexedSeq
+  }
+}
+
+case class StepWiseConstraint[F <: CronField](
+    base: DivisibleConstraint[F],
+    step: Int,
+    unit: CronUnit[F]
+) extends Constraint[F]
+object StepWiseConstraint {
+  implicit def stepWiseConstraintRange[F <: CronField](
+      implicit R: Enumerated[DivisibleConstraint[F]]
+  ): Enumerated[StepWiseConstraint[F]] = new Enumerated[StepWiseConstraint[F]] {
+    def range(c: StepWiseConstraint[F]) = {
+      val elements = Stream
+        .iterate[Either[StepError, (Int, Int)]](Right(R.min(c.base) -> 0)) {
+          _.flatMap { case (v, _) => R.step(c.base)(v, c.step) }
+        }
+        .map(_.toOption)
+        .flatten
+        .takeWhile(_._2 < 1)
+        .map(_._1)
+
+      elements.toVector
+    }
+  }
+}
+
+// Nodes
+
+sealed trait CronNode[F <: CronField]
+
+case class RangeNode[F <: CronField](constraint: Constraint[F]) extends CronNode[F]
+object RangeNode {
+
+  implicit def nodeSteppable[F <: CronField, DT](
+      implicit S: Steppable[Constraint[F], Int],
+      DT: IsDateTime[DT]
+  ) = new Steppable[RangeNode[F], DT] {
+    def step(node: RangeNode[F], from: DT, step: Step): Either[StepError, (DT, Int)] =
+      for {
+        currValue             <- DT.get(from, node.constraint.unit.field)
+        (newValue, carryOver) <- S.step(node.constraint, currValue, step)
+        newResult             <- DT.set(from, node.constraint.unit.field, newValue)
+      } yield (newResult, carryOver)
+  }
+
+}
+
+case object LastDayOfMonth extends CronNode[CronField.DayOfMonth]
+case class NthDayOfWeek(nth: Int) extends CronNode[CronField.DayOfWeek]
+case class NthDayOfMonth(nth: Int) extends CronNode[CronField.DayOfMonth]
+case class NthDayOnMthWeek(nth: Int, mth: Int) extends CronNode[CronField.DayOfMonth]
