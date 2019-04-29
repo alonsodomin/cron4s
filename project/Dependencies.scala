2bde0c5c581811a0fb8ff2e7e7d31ca6ad681271
Scala steward <43047562+scala-steward@users.noreply.github.com>
2019-04-29 18:19:23 +0200

    Update joda-convert to 2.2.1 (#160)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -22,7 +22,7 @@ object Dependencies {
     val contextual = "1.1.0"
 
     val jodaTime    = "2.10.1"
-    val jodaConvert = "2.2.0"
+    val jodaConvert = "2.2.1"
 
     val momentjs      = "0.8.1"
     val scalaJavaTime = "2.0.0-RC1"
