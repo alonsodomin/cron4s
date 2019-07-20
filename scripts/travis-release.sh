940d175b6a977f25b2757ceb99087c48cc0bbbb7
Antonio Alonso Dominguez <antoniodominguez@rentalcars.com>
2019-07-20 10:52:07 +0100

    Publish docs only on 2.12

--- scripts/travis-release.sh
+++ scripts/travis-release.sh
@@ -4,7 +4,7 @@ set -e
 
 sbt ci-release
 
-if [ "$TRAVIS_BRANCH" = "master" ] && [ ! -z "$TRAVIS_TAG" ]; then
+if [ "$TRAVIS_BRANCH" = "master" ] && [ ! -z "$TRAVIS_TAG" ] && [ "$TRAVIS_SCALA_VERSION" = "2.12.8" ]; then
     echo "Publishing documentation..."
     sbt docs/publishMicrosite
 fi
