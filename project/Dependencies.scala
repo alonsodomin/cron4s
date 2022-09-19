7261ff9827247262ee8612584b35c1aa701cb160
Scala Steward <scala_steward@virtuslab.com>
2022-09-19 12:36:38 +0000

    Update circe-core, circe-testing to 0.14.3

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -19,7 +19,7 @@ object Dependencies {
     val shapeless  = "2.3.7"
     val discipline = "1.0.1"
     val decline    = "1.4.0"
-    val circe      = "0.14.1"
+    val circe      = "0.14.3"
     val parserc    = "1.1.2"
     val doobie     = "0.13.4"
 
